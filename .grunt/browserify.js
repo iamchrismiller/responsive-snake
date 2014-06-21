0c7b59b2ae0e132bd1c8076b6954ca324f69db1f
Chris Miller <cmille142@gmail.com>
2014-06-20 21:04:01 -0700

    updated build to include banner

--- .grunt/browserify.js
+++ .grunt/browserify.js
@@ -1,6 +1,7 @@
 module.exports = {
   build : {
     files : {
+
       'build/snake.build.js' : ['src/main.js']
     }
   }
--- .grunt/uglify.js
+++ .grunt/uglify.js
@@ -1,6 +1,10 @@
 module.exports = {
 
   build: {
+    options : {
+      banner: '/*! <%= package.name %> - v<%= package.version %> - ' +
+        '<%= grunt.template.today("yyyy-mm-dd") %> */\n'
+    },
     files: {
       'build/snake.build.min.js': ['build/snake.build.js']
     }
